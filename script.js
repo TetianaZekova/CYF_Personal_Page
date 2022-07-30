4ea9f4a0b5515aac2ccf5ada684582a698cd6edb
Tetiana Zekova <32105279+webdesign-new@users.noreply.github.com>
2022-07-30 20:34:58 +0100

    Personal Page_Hamburger menu, Picture Carousel
    This is my deployed personal website 
    I updated it with 2 CYF Blocks:
    1. Carousel
    2. Hamburger menu
    
    User story
    Image carousel project:
    As a user, I can click a “current dot”  button so that I can cycle  through a set of images 
    
    As a user, I can click a “previous dot” button so that I can cycle  back through a set of images 
    
    As a user, I can cycle through a set of images infinitely using either the “next dot” or “previous dot” buttons.
    
    Hamburger menu 
    As a user, I can click the “hamburger icon”  and can see a drop-down menu with a list of links
    
    As a user, I can click the “close icon” and hide the drop-down menu.

---
+++ script.js
@@ -0,0 +1,58 @@
+const popup = document.querySelector("#popup");
+const body = document.body;
+const menu = document.querySelector("#menu").cloneNode(1);
+hamb.addEventListener("click", hambHandler);
+function hambHandler(e) {
+  e.preventDefault();
+  
+  popup.classList.toggle("open");
+  hamb.classList.toggle("active");
+  body.classList.toggle("noscroll");
+  renderPopup();
+}
+ 
+
+
+
+
+function renderPopup() {
+  popup.appendChild(menu);
+}
+const links = Array.from(menu.children);
+ 
+links.forEach((link) => {
+  link.addEventListener("click", closeOnClick);
+});
+
+function closeOnClick() {
+  popup.classList.remove("open");
+  hamb.classList.remove("active");
+  body.classList.remove("noscroll");
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
